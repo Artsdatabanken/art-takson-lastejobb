4d051baf6d0bc558d491f5c9a2a8bcd9c8973e10
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-09-29 22:30:06 +0200

    Kode på alle elementer

--- stages/transform/40/type.js
+++ stages/transform/40/type.js
@@ -1,11 +1,12 @@
-const { io, json, url } = require("lastejobb");
+const { io, url } = require("lastejobb");
 
 let tre = io.lesDatafil("map2");
 const toplevel = io.lesDatafil("art-takson-ubehandlet/type");
 toplevel.forEach(node => {
   const kode = node.kode;
   if (tre[kode]) throw new Error("Duplikat: " + kode);
+  delete node.kode;
   tre[kode] = node;
 });
 new url(tre).assignUrls();
-io.skrivBuildfil(__filename, json.objectToArray(tre));
+io.skrivBuildfil(__filename, tre);
