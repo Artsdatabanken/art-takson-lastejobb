798499c6c65a766e61e596ef00837ee0521b96a0
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-11-11 09:23:48 +0100

    Karakteristisk art

--- stages/download/10/ubehandlet.js
+++ stages/download/10/ubehandlet.js
@@ -2,5 +2,5 @@ const { git } = require("lastejobb");
 
 git.clone(
   "https://github.com/Artsdatabanken/art-takson-ubehandlet.git",
-  "data/art-takson-ubehandlet"
+  "temp/art-takson-ubehandlet"
 );
---
+++ stages/transform/50_karakteristisk.js
@@ -0,0 +1,33 @@
+const { io, log } = require("lastejobb");
+
+const inn = io.lesDatafil("art-takson-ubehandlet/karakteristisk_art");
+const takson = io.lesBuildfil("type").items;
+
+const kode2Takson = takson.reduce((acc, e) => {
+  acc[e.kode] = e;
+  return acc;
+}, {});
+
+const sciName2Takson = takson.reduce((acc, e) => {
+  acc[e.tittel.sn.toLowerCase()] = e;
+  return acc;
+}, {});
+
+const r = {};
+
+inn.arter.forEach(karakteristisk => {
+  var kart = sciName2Takson[karakteristisk.toLowerCase()];
+  if (!kart)
+    return log.warn(
+      "Finner ikke karakteristisk art i taksonomi: " + karakteristisk
+    );
+  var art = kart;
+  while (art.foreldre.length > 0) {
+    const forelder = art.foreldre[0];
+    if (r[forelder]) return; // En mer karakteristisk art har allerede tatt plassen
+    r[forelder] = kart.kode;
+    art = kode2Takson[forelder];
+  }
+});
+
+io.skrivBuildfil("karakteristisk_art", r);
